185a3198acd4ad188744e58ec440be9f822f900f
kanupriya-ishu <kanupriya2512@gmail.com>
2022-02-12 22:18:33 +0530

    Created header

---
+++ react-blog/src/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css"
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <div className="headerTitles">
+            <span className="headerTitleSm">React & Node</span>
+            <span className="headerTitleLg">Blog</span>
+        </div>
+        <img className="headerImg" src="https://media.istockphoto.com/photos/spring-beautiful-background-with-green-juicy-young-foliage-and-empty-picture-id1301592032?b=1&k=20&m=1301592032&s=170667a&w=0&h=TwSiOgSX3Kc8hRKREuPAg3SQfhLngTMQqAI-xldjuvg=" alt=""></img>
+    </div>
+  )
+}
