d74f133bc104975da25cad95b4a4150c3baa5add
kanupriya-ishu <kanupriya2512@gmail.com>
2022-02-13 15:08:54 +0530

    Node js and mongodb connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify:true
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.use("/", (req, res) => {
     console.log("Hey this is main url");
