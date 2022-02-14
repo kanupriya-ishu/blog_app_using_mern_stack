573e06f0c65cfb39179b4fabeb0d3709f7b876a1
kanupriya-ishu <kanupriya2512@gmail.com>
2022-02-14 15:52:05 +0530

    Displaying all posts on the homepage

--- react-blog/src/pages/home/Home.jsx
+++ react-blog/src/pages/home/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   useEffect(() => {
     const fetchPosts = async () => {
       const res = await axios.get("/posts");
-      console.log(res.data);
+      setPosts(res.data);
     };
     fetchPosts();
   }, []);
@@ -20,7 +20,7 @@ export default function Home() {
     <>
     <Header/>
     <div className='home'>
-      <Posts />
+      <Posts posts={posts}/>
       <Sidebar />
     </div>
     </>
