125d3a03a35e025c9f1d8ae65e735f64db344c5a
kanupriya-ishu <kanupriya2512@gmail.com>
2022-03-08 14:57:00 +0530

    Logout using Context API

--- react-blog/src/components/topbar/TopBar.jsx
+++ react-blog/src/components/topbar/TopBar.jsx
@@ -4,7 +4,12 @@ import { Context } from "../../context/Context";
 import "./topbar.css";
 
 export default function Topbar() {
-  const {user} = useContext(Context);
+  const { user, dispatch } = useContext(Context)
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
   return (
     <div className="top">
       <div className="topLeft">
@@ -27,7 +32,9 @@ export default function Topbar() {
               WRITE
             </Link>
           </li>
-          {user && <li className="topListItem">LOGOUT</li>}
+          <li className="topListItem" onClick={handleLogout}>
+            {user && "LOGOUT"}
+          </li>
         </ul>
       </div>
       <div className="topRight">
@@ -35,7 +42,7 @@ export default function Topbar() {
           <Link className="link" to="/settings">
             <img
               className="topImg"
-              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              src={user.profilePic}
               alt=""
             />
           </Link>
